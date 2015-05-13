9fd84e295565d689c9955b5b2f4be67f431d263d
JavierYanezSepulveda <javierandres.y.s@gmail.com>
2015-05-13 10:21:14 -0300

    Tarea 3

---
+++ tarea3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int a=12, b=3, c=4, d=1, e=2, f=8;
+    int x=0, y=0, z=0, w=0, suerte=0;
+    int basura;
+
+    printf("Este es el programa para ver su numero de la suerte:\n");
+    printf("Ingrese su dia de nacimiento:\n");
+    scanf("%d", &basura);
+    x=a+b+b;
+    x++;
+    y=x*d;
+    y--;
+    z=(y-a)-(b-d);
+    w=z*f;
+    suerte=w/e;
+    printf("Su numero de la suerte es %d\n", suerte);
+    printf("FIN");
+
+
+}
