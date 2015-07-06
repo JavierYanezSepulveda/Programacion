f08b4c12aaf202676ac27e32bbcc934ea2415837
JavierYanezSepulveda <javierandres.y.s@gmail.com>
2015-07-05 23:45:15 -0300

    Update paf.c

--- paf.c
+++ paf.c
@@ -1,4 +1,4 @@
-//Javier Andrés Yáñez Sepúlveda
+//Javier AndrÃ©s YÃ¡Ã±ez SepÃºlveda
 //RUT: 18731299-K
 #include<stdio.h>
 #include<string.h>
@@ -6,24 +6,25 @@
 int main(){
     FILE *entrada=fopen("entrada.ent", "r");
     FILE *salida=fopen("salida.sal", "w");
-    if(entrada==NULL) return 1;
-    int i, P=1, x, resta[1], control;
-    char N[MAX];                                //Tamaño del arreglo
+    int i, P=1, x, resta[1], N;                 //N es el tamaÃ±o del arreglo control
+    char control[MAX];                          //arreglo de todas las variables
     int *p1, *p2, *p3;                          //Punteros de las variables enteras
     int cabeza[1], cola[1];                     //Arreglos resultantes de la division de la cadena
     char *num;                                  //Puntero de la cadena
-    num=&N[0];
+    num=&control[0];
     p1=&cabeza[0];
     p2=&cola[0];
     p3=&resta[0];
-    fscanf(entrada, "%d %s", &control, &N);     //lectura del archivo de texto
+    if(entrada==NULL) return 1;
+    fscanf(entrada, "%d %s", &N, &control);     //lectura del archivo de texto
+    if(N>=3){
     cabeza[0]=*num-48;                          //transformacion del caracter a numero
-    for(i=1; i<control; i++){
+    for(i=1; i<N; i++){
         cola[0]=cola[0]+(*(num+i)-48);
     }
     x=*p1-*p2;
     if(x<0) x=x*(-1);
-    while(P<control){                           //variable que designa el punto de corte "P"
+    while(P<N){                           //variable que designa el punto de corte "P"
         *p1=*p1+(*(num+P) -48);
         *p2=*p2-(*(num+P)-48);
         *p3=*p1-*p2;
@@ -35,4 +36,6 @@ int main(){
      fprintf(salida, "%d\n", x);
      fclose(salida);
      fclose(entrada);
+    }
+    else printf("La cantidad de elementos de un arreglo debe ser como mÃ­nimo tres\nCambie su entrada y reinicie el programa\n");
 }
